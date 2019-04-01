148c58c67e7796b1f0ae5160284df462e704d3c3
fjwcoder <fjwcoder@gmail.com>
2019-04-01 11:07:05 +0800

    添加图片上传

--- siteinfo.js
+++ siteinfo.js
@@ -3,5 +3,7 @@ module.exports = {
   siteroot: "https://wxapp.dxyxshop.com/api.php/", // 必填: api地址，结尾要带/
   // siteroot: "http://mamiv2.server/api.php/", // 必填: api地址，结尾要带/
   // siteroot: "http://mall.dxyxshop.com/index.php/index.php?s=/api/", // 必填: api地址，结尾要带/
+  uploadroot: "https://wxapp.dxyxshop.com/api.php/", // 文件上传地址
+  // uploadroot: "http://mamiv2.server/api.php/", // 文件上传地址
   uniacid: "10001", // 默认即可，勿填
 };
