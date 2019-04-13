471bdee5edf28e4ee0a76152861882bb48e1749f
WIN-91BH5RD2QQ9\Administrator <769612131@qq.com>
2019-04-13 17:12:36 +0800

    保险详情页面

--- pages/order/preview.js
+++ pages/order/preview.js
@@ -48,7 +48,12 @@ Page({
     in_us_name: '',
     in_us_idcard: '',
     in_baby_name: '',
-    in_baby_idcard: ''
+    in_baby_idcard: '',
+
+    insurance_list: '',
+
+    price_sum: '', //保费和
+    pay_limit_sum: '', //保额和
   },
 
   /**
@@ -66,18 +71,20 @@ Page({
 
     this.data.insurance_id = options.insurance_id; // 保险ID
 
-   
+
     // 获取商品（保险）详细信息
-    var strs = new Array();
-    strs = options.list.split(",").map(Number);
-    this.getInsuranceDetail(parseInt(options.baby_id),strs);
+    // var strs = new Array();
+    // strs = options.list.split(",").map(Number);
+    this.getInsuranceDetail(parseInt(options.baby_id), options.list);
     // console.log(options);
     // return false;
 
     //获取自定义保单列表
     this.getInspolicyList(parseInt(options.baby_id));
-   
-
+    this.setData({
+      list: options.list,
+      baby_id:options.baby_id
+    })
   },
 
 
@@ -145,13 +152,53 @@ Page({
   /**
    * 通过保险ID链获取保险信息
    */
- getInsuranceDetail:function(babyid,_list){
-   console.log(babyid);
-   console.log(_list);
-    App._post_form('insurance/getinsurancebylist',{baby_id:babyid,list:_list}, function (result) {
-        console.log(result)
+  getInsuranceDetail: function (babyid, _list) {
+    let _this = this;
+    console.log(babyid);
+    console.log(_list);
+    App._post_form('insurance/getinsurancebylist', {
+      baby_id: babyid,
+      list: _list
+    }, function (result) {
+      //console.log(result)
+      var price_sum = 0; //求保费和
+      for (var k = 0; k < result.data.length; k++) {
+        price_sum += parseInt(result.data[k].price);
+      }
+      var pay_limit_sum = 0; //求保额和
+      for (var k = 0; k < result.data.length; k++) {
+        pay_limit_sum += parseInt(result.data[k].pay_limit);
+      }
+      _this.setData({
+        insurance_list: result.data,
+        price_sum: price_sum,
+        pay_limit_sum: pay_limit_sum
+      })
     })
- },
+    //console.log(_this.data.insurance_list[0])
+  },
+  /**
+   * 表单验证
+   */
+  validation: function (values) {
+    if (values.in_us_name !== values.user_name || values.in_us_name === '') {
+      this.data.error = '校验投保人名称有误';
+      return false;
+    }
+    if (values.in_us_idcard !== values.user_id_card || values.in_us_idcard === '') {
+      this.data.error = '校验投保人身份证号有误';
+      return false;
+    }
+    if (values.in_baby_name !== values.baby_name || values.in_baby_name === '') {
+      this.data.error = '校验被保人名称有误';
+      return false;
+    }
+    if (values.in_baby_idcard !== values.baby_id_card || values.in_baby_idcard === '') {
+      this.data.error = '校验被保人身份证号有误';
+      return false;
+    }
+    return true;
+  },
 
   /*
    * 获取保险详情
@@ -175,18 +222,21 @@ Page({
    * 表单提交
    */
   saveData: function (e) {
-
-    let _this = this,
-      values = _this.data.define_list[_this.data.index]
-
-    //保险id赋值到values
-    values.insurance_id = parseInt(_this.data.insurance_id)
-    //user_token赋值到values
-    values.user_token = App.getGlobalData('user_token');
-
-    console.log(values);
+    let _this = this;
+    var values;
+   
+    console.log (_this.data.list);
+    
+    console.log (App.getGlobalData('user_token'));
+  
+    console.log(parseInt( _this.data.baby_id));
 
 
+    // 表单验证
+    if (!_this.validation(_this.data)) {
+      App.showError(_this.data.error);
+      return false;
+    }
     // 按钮禁用
     _this.setData({
       disabled: true
